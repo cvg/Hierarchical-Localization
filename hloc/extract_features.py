ccb7a08e91e170ad9c4e6d64c6067f7546e9ac47
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2020-09-15 23:56:51 +0200

    Add argument for fp16 export to save disk space

--- hloc/extract_features.py
+++ hloc/extract_features.py
@@ -165,5 +165,6 @@ if __name__ == '__main__':
     parser.add_argument('--export_dir', type=Path, required=True)
     parser.add_argument('--conf', type=str, default='superpoint_aachen',
                         choices=list(confs.keys()))
+    parser.add_argument('--as_half', action='store_true')
     args = parser.parse_args()
-    main(confs[args.conf], args.image_dir, args.export_dir)
+    main(confs[args.conf], args.image_dir, args.export_dir, args.as_half)
