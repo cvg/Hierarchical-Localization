fb3ab34b9ac5732db199ff6759d19aef7dfbf635
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2021-01-29 10:09:47 +0100

    Update retrieval for minimal changes

--- hloc/extract_features.py
+++ hloc/extract_features.py
@@ -14,16 +14,6 @@ from .utils.base_model import dynamic_load
 from .utils.tools import map_tensor
 
 
-import torch.utils.data
-from PIL import Image
-
-import torchvision
-normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
-transform = torchvision.transforms.Compose([
-                                       torchvision.transforms.ToTensor(),
-                                       normalize])
-
 '''
 A set of standard configurations that can be directly selected from the command
 line using their name. Each is a dictionary with the following entries:
@@ -83,13 +73,12 @@ confs = {
         },
     },
     'dir': {
-        'output': 'feats-dir-2048d',
+        'output': 'global-feats-dir',
         'model': {
             'name': 'dir',
         },
         'preprocessing': {
-            'grayscale': False,
-            'global_descriptor': True,
+            'resize_max': None,
         },
     },
 }
@@ -106,8 +95,8 @@ class ImageDataset(torch.utils.data.Dataset):
     def __init__(self, root, conf):
         self.conf = conf = SimpleNamespace(**{**self.default_conf, **conf})
         self.root = root
-        self.paths = []
 
+        self.paths = []
         for g in conf.globs:
             self.paths += list(Path(root).glob('**/'+g))
         if len(self.paths) == 0:
@@ -117,41 +106,31 @@ class ImageDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         path = self.paths[idx]
-
-        if self.conf.global_descriptor:
-            image = np.asarray(Image.open(str(self.root / path)).convert('RGB'))
-            size = image.shape[:2][::-1]
-            if self.conf.grayscale:
-                image = image[None]
-            else:
-                image = image
-            image = transform(image)
-
+        if self.conf.grayscale:
+            mode = cv2.IMREAD_GRAYSCALE
         else:
-            if self.conf.grayscale:
-                mode = cv2.IMREAD_GRAYSCALE
-            else:
-                mode = cv2.IMREAD_COLOR
-            image = cv2.imread(str(self.root / path), mode)
-            if not self.conf.grayscale:
-                image = image[:, :, ::-1]  # BGR to RGB
-            if image is None:
-                raise ValueError(f'Cannot read image {str(path)}.')
-            image = image.astype(np.float32)
-            size = image.shape[:2][::-1]
-            w, h = size
+            mode = cv2.IMREAD_COLOR
+        image = cv2.imread(str(self.root / path), mode)
+        if not self.conf.grayscale:
+            image = image[:, :, ::-1]  # BGR to RGB
+        if image is None:
+            raise ValueError(f'Cannot read image {str(path)}.')
+        image = image.astype(np.float32)
+        size = image.shape[:2][::-1]
+        w, h = size
 
-            if self.conf.resize_max and (self.conf.resize_force
-                                         or max(w, h) > self.conf.resize_max):
-                scale = self.conf.resize_max / max(h, w)
-                h_new, w_new = int(round(h*scale)), int(round(w*scale))
-                image = cv2.resize(
-                    image, (w_new, h_new), interpolation=cv2.INTER_LINEAR)
-            if self.conf.grayscale:
-                image = image[None]
-            else:
-                image = image.transpose((2, 0, 1))  # HxWxC to CxHxW
-            image = image / 255.
+        if self.conf.resize_max and (self.conf.resize_force
+                                     or max(w, h) > self.conf.resize_max):
+            scale = self.conf.resize_max / max(h, w)
+            h_new, w_new = int(round(h*scale)), int(round(w*scale))
+            image = cv2.resize(
+                image, (w_new, h_new), interpolation=cv2.INTER_LINEAR)
+
+        if self.conf.grayscale:
+            image = image[None]
+        else:
+            image = image.transpose((2, 0, 1))  # HxWxC to CxHxW
+        image = image / 255.
 
         data = {
             'name': str(path),
@@ -172,23 +151,21 @@ def main(conf, image_dir, export_dir, as_half=False):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     Model = dynamic_load(extractors, conf['model']['name'])
     model = Model(conf['model']).eval().to(device)
+
     loader = ImageDataset(image_dir, conf['preprocessing'])
-    loader = torch.utils.data.DataLoader(loader)
+    loader = torch.utils.data.DataLoader(loader, num_workers=1)
 
     feature_path = Path(export_dir, conf['output']+'.h5')
     feature_path.parent.mkdir(exist_ok=True, parents=True)
     feature_file = h5py.File(str(feature_path), 'a')
-    print('feature_file', feature_file)
 
     for data in tqdm(loader):
         if data['name'][0] in feature_file:
             continue
 
-        if conf['model']['name'] == 'dir':
-            pred = model(data['image'])
-        else:
-            pred = model(map_tensor(data, lambda x: x.to(device)))
+        pred = model(map_tensor(data, lambda x: x.to(device)))
         pred = {k: v[0].cpu().numpy() for k, v in pred.items()}
+
         pred['image_size'] = original_size = data['original_size'][0].numpy()
         if 'keypoints' in pred:
             size = np.array(data['image'].shape[-2:][::-1])
@@ -200,9 +177,11 @@ def main(conf, image_dir, export_dir, as_half=False):
                 dt = pred[k].dtype
                 if (dt == np.float32) and (dt != np.float16):
                     pred[k] = pred[k].astype(np.float16)
+
         grp = feature_file.create_group(data['name'][0])
         for k, v in pred.items():
             grp.create_dataset(k, data=v)
+
         del pred
 
     feature_file.close()
@@ -220,4 +199,3 @@ if __name__ == '__main__':
     parser.add_argument('--as_half', action='store_true')
     args = parser.parse_args()
     main(confs[args.conf], args.image_dir, args.export_dir, args.as_half)
-
--- hloc/extractors/dir.py
+++ hloc/extractors/dir.py
@@ -3,75 +3,76 @@ from pathlib import Path
 import subprocess
 import logging
 import torch
-
 from zipfile import ZipFile
 import os
 
 from ..utils.base_model import BaseModel
 
 dir_path = Path(__file__).parent / '../../third_party/deep-image-retrieval'
-
 sys.path.append(str(dir_path))
+os.environ['DB_ROOT'] = ''  # required by dirtorch
 
 from dirtorch import nets as nets
 from dirtorch.utils import common
+from dirtorch.extract_features import load_model
 
-# This doesn't work directly, need to comment lines 17, 19, and 20 from `dirtorch/extract_features.py`
-#from dirtorch.extract_features import load_model
-
-def load_model(path, iscuda=True):
-    checkpoint = common.load_checkpoint(path, iscuda)
-    net = nets.create_model(pretrained="", **checkpoint['model_options'])
-    net = common.switch_model_to_cuda(net, iscuda, checkpoint)
-    net.load_state_dict(checkpoint['state_dict'])
-    net.preprocess = checkpoint.get('preprocess', net.preprocess)
-    if 'pca' in checkpoint:
-        net.pca = checkpoint.get('pca')
-    return net
 
 class DIR(BaseModel):
     default_conf = {
         'model_name': 'Resnet-101-AP-GeM',
         'checkpoint': dir_path / 'dirtorch/data',
-        'whiten': 'Landmarks_clean',
-        'whitenp': 0.25,
-        'whitenv': None,
-        'whitenm': 1.0,
+        'whiten_name': 'Landmarks_clean',
+        'whiten_params': {
+            'whitenp': 0.25,
+            'whitenv': None,
+            'whitenm': 1.0,
+        },
         'pooling': 'gem',
         'gemp': 3,
     }
     required_inputs = ['image']
 
+    dir_models = {
+        'Resnet-101-AP-GeM': 'https://docs.google.com/uc?export=download&id=1UWJGDuHtzaQdFhSMojoYVQjmCXhIwVvy',
+    }
+
     def _init(self, conf):
-        dir_models = {
-            'Resnet-101-AP-GeM': 'https://docs.google.com/uc?export=download&id=1UWJGDuHtzaQdFhSMojoYVQjmCXhIwVvy',
-        }
-
-        model_file = dir_path / 'dirtorch/data' / str(conf['model_name']+'.pt')
-
-        if not model_file.exists():
-            model_file.parent.mkdir(exist_ok=True)
-            cmd = ['wget', '--no-check-certificate', '-r', dir_models[conf['model_name']],
-                   '-O', str(model_file)+'.zip']
+        checkpoint = dir_path / 'dirtorch/data' / str(conf['model_name']+'.pt')
+        if not checkpoint.exists():
+            checkpoint.parent.mkdir(exist_ok=True)
+            link = self.dir_models[conf['model_name']]
+            cmd = ['wget', '--no-check-certificate', '-r', link,
+                   '-O', str(checkpoint)+'.zip']
             ret = subprocess.call(cmd)
-            zf = ZipFile(str(model_file)+'.zip', 'r')
-            zf.extractall(model_file.parent)
+            zf = ZipFile(str(checkpoint)+'.zip', 'r')
+            zf.extractall(checkpoint.parent)
             zf.close()
-            os.remove(str(model_file)+'.zip')
+            os.remove(str(checkpoint)+'.zip')
             if ret != 0:
                 logging.warning(
                     f'Cannot download the DIR model with `{cmd}`.')
                 exit(ret)
 
-        self.net = load_model(conf['checkpoint'] / str(conf['model_name']+'.pt'))
-
-        if conf['whiten']:
-            self.net.pca = self.net.pca[conf['whiten']]
-        else:
-            self.net.pca = None
+        self.net = load_model(checkpoint, False)  # first load on CPU
+        if conf['whiten_name']:
+            assert conf['whiten_name'] in self.net.pca
 
     def _forward(self, data):
-        desc = self.net(data)
+        image = data['image']
+        assert image.shape[1] == 3
+        mean = self.net.preprocess['mean']
+        std = self.net.preprocess['std']
+        image = image - image.new_tensor(mean)[:, None, None]
+        image = image / image.new_tensor(std)[:, None, None]
+
+        desc = self.net(image)
+        desc = desc.unsqueeze(0)  # batch dimension
+        if self.conf['whiten_name']:
+            pca = self.net.pca[self.conf['whiten_name']]
+            desc = common.whiten_features(
+                    desc.cpu().numpy(), pca, self.conf['whiten_params'])
+            desc = torch.from_numpy(desc)
+
         return {
-            'global_descriptor': desc[None],
+            'global_descriptor': desc,
         }
