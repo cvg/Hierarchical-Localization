1fdf655208fe8d60ae9253d7714d4a98c26cd3cf
Marvin <marvin.chancan@gmail.com>
2021-02-02 18:26:54 +1000

    Add global descriptor extraction with DIR (#41)
    https://github.com/naver/deep-image-retrieval/
    
    Co-authored-by: Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>

--- hloc/extract_features.py
+++ hloc/extract_features.py
@@ -72,6 +72,15 @@ confs = {
             'resize_max': 1600,
         },
     },
+    'dir': {
+        'output': 'global-feats-dir',
+        'model': {
+            'name': 'dir',
+        },
+        'preprocessing': {
+            'resize_max': None,
+        },
+    },
 }
 
 
