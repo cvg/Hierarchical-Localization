b55fac29c361becf486b9fe170e12b32a7116c0d
Lixin Xue <lixxue@ethz.ch>
2021-02-12 16:12:26 +0100

    rename conf keys and resolve error&warnings based on flake8 (E402 remained)

--- hloc/extractors/r2d2.py
+++ hloc/extractors/r2d2.py
@@ -1,6 +1,5 @@
 import sys
 from pathlib import Path
-import torch
 import torchvision.transforms as tvf
 
 from ..utils.base_model import BaseModel
@@ -12,42 +11,46 @@ from extract import load_network, NonMaxSuppression, extract_multiscale
 
 class R2D2(BaseModel):
     default_conf = {
-        'checkpoint_name': 'r2d2_WASF_N16.pt',
-        'top-k': 5000,
-
-        'scale-f': 2**0.25,
-        'min-size': 256,
-        'max-size': 1024,
-        'min-scale': 0,
-        'max-scale': 1,
-
-        'reliability-thr': 0.7,
-        'repetability-thr': 0.7,
+        'model_name': 'r2d2_WASF_N16.pt',
+        'max_keypoints': 5000,
+        'scale_factor': 2**0.25,
+        'min_size': 256,
+        'max_size': 1024,
+        'min_scale': 0,
+        'max_scale': 1,
+        'reliability_threshold': 0.7,
+        'repetability_threshold': 0.7,
     }
     required_inputs = ['image']
 
     def _init(self, conf):
-        model_fn = r2d2_path / "models" / conf['checkpoint_name']
-        self.norm_rgb = tvf.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        model_fn = r2d2_path / "models" / conf['model_name']
+        self.norm_rgb = tvf.Normalize(mean=[0.485, 0.456, 0.406],
+                                      std=[0.229, 0.224, 0.225])
         self.net = load_network(model_fn)
-        self.detector = NonMaxSuppression(rel_thr=conf['reliability-thr'], 
-            rep_thr=conf['repetability-thr'])
-        
+        self.detector = NonMaxSuppression(
+            rel_thr=conf['reliability_threshold'],
+            rep_thr=conf['repetability_threshold']
+        )
+
     def _forward(self, data):
         img = data['image']
-        img = self.norm_rgb(img[0])[None]
+        img = self.norm_rgb(img)
 
-        xys, desc, scores = extract_multiscale(self.net, img, self.detector,
-                scale_f = self.conf['scale-f'],
-                min_size = self.conf['min-size'],
-                max_size = self.conf['max-size'],
-                min_scale = self.conf['min-scale'],
-                max_scale = self.conf['max-scale'],
+        xys, desc, scores = extract_multiscale(
+            self.net, img, self.detector,
+            scale_f=self.conf['scale_f'],
+            min_size=self.conf['min_size'],
+            max_size=self.conf['max_size'],
+            min_scale=self.conf['min_scale'],
+            max_scale=self.conf['max_scale'],
         )
-        idxs = scores.argsort()[-self.conf['top-k'] or None:]
+        idxs = scores.argsort()[-self.conf['max_points'] or None:]
         xy = xys[idxs, :2]
         desc = desc[idxs].t()
         scores = scores[idxs]
 
-        pred = {'keypoints': xy[None], 'descriptors': desc[None], 'scores': scores[None]}
+        pred = {'keypoints': xy[None],
+                'descriptors': desc[None],
+                'scores': scores[None]}
         return pred
