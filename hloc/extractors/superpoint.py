fd2e6c2406782ef30368a41a6544f66a17e97150
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2021-12-28 19:18:09 +0100

    Finally fix the SuperPoint descriptor sampling (optional)

--- hloc/extractors/superpoint.py
+++ hloc/extractors/superpoint.py
@@ -29,16 +29,34 @@ def sample_descriptors(keypoints, descriptors, s: int = 8):
 superpoint.sample_descriptors = sample_descriptors
 
 
+# The original keypoint sampling is incorrect. We patch it here but
+# we don't fix it upstream to not impact exisiting evaluations.
+def sample_descriptors_fix_sampling(keypoints, descriptors, s: int = 8):
+    """ Interpolate descriptors at keypoint locations """
+    b, c, h, w = descriptors.shape
+    keypoints = (keypoints + 0.5) / (keypoints.new_tensor([w, h]) * s)
+    keypoints = keypoints * 2 - 1  # normalize to (-1, 1)
+    descriptors = torch.nn.functional.grid_sample(
+        descriptors, keypoints.view(b, 1, -1, 2),
+        mode='bilinear', align_corners=False)
+    descriptors = torch.nn.functional.normalize(
+        descriptors.reshape(b, c, -1), p=2, dim=1)
+    return descriptors
+
+
 class SuperPoint(BaseModel):
     default_conf = {
         'nms_radius': 4,
         'keypoint_threshold': 0.005,
         'max_keypoints': -1,
         'remove_borders': 4,
+        'fix_sampling': False,
     }
     required_inputs = ['image']
 
     def _init(self, conf):
+        if conf['fix_sampling']:
+            superpoint.sample_descriptors = sample_descriptors_fix_sampling
         self.net = superpoint.SuperPoint(conf)
 
     def _forward(self, data):
