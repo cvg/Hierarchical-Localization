faaac109f8e9ad48178d3a484027f2262d332b9b
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2022-01-01 19:23:10 +0100

    Add __version__

--- hloc/__init__.py
+++ hloc/__init__.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 
+__version__ = '1.1'
+
 logging.basicConfig(stream=sys.stdout,
                     format='[%(asctime)s %(levelname)s] %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup
 
 description = ['Tools and baselines for visual localization and mapping']
 
-with open(str(Path(__file__).parent / 'README.md'), 'r', encoding='utf-8') as f:
+root = Path(__file__).parent
+with open(str(root / 'README.md'), 'r', encoding='utf-8') as f:
     readme = f.read()
+with open(str(root / 'hloc/__init__.py'), 'r') as f:
+    version = eval(f.read().split('__version__ = ')[1].split()[0])
 
 dependencies = [
     'torch>=1.1',
@@ -20,7 +23,7 @@ dependencies = [
 
 setup(
     name='hloc',
-    version='1.1',
+    version=version,
     packages=['hloc'],
     python_requires='>=3.6',
     install_requires=dependencies,
