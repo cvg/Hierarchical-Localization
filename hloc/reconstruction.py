ab207174515be0569282e502e533800b87be2f81
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2022-04-15 09:32:28 +0200

    Allow access to default camera model

--- hloc/reconstruction.py
+++ hloc/reconstruction.py
@@ -22,14 +22,15 @@ def create_empty_db(database_path):
     db.close()
 
 
-def import_images(image_dir, database_path, camera_mode, image_list=None):
+def import_images(image_dir, database_path, camera_mode, image_list=None,
+                  camera_model=None):
     logger.info('Importing images into the database...')
     images = list(image_dir.iterdir())
     if len(images) == 0:
         raise IOError(f'No images found in {image_dir}.')
     with pycolmap.ostream():
         pycolmap.import_images(database_path, image_dir, camera_mode,
-                               image_list=image_list or [])
+                               camera_model or '', image_list or [])
 
 
 def get_image_ids(database_path):
@@ -78,7 +79,8 @@ def run_reconstruction(sfm_dir, database_path, image_dir, verbose=False):
 def main(sfm_dir, image_dir, pairs, features, matches,
          camera_mode=pycolmap.CameraMode.AUTO, verbose=False,
          skip_geometric_verification=False, min_match_score=None,
-         image_list: Optional[List[str]] = None):
+         image_list: Optional[List[str]] = None,
+         camera_model: Optional[str] = None):
 
     assert features.exists(), features
     assert pairs.exists(), pairs
@@ -88,7 +90,7 @@ def main(sfm_dir, image_dir, pairs, features, matches,
     database = sfm_dir / 'database.db'
 
     create_empty_db(database)
-    import_images(image_dir, database, camera_mode, image_list)
+    import_images(image_dir, database, camera_mode, image_list, camera_model)
     image_ids = get_image_ids(database)
     import_features(image_ids, database, features)
     import_matches(image_ids, database, pairs, matches,
