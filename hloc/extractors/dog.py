a45dee521eb192c67ea07d91effa6a768f81e306
Paul-Edouard Sarlin <Skydes@users.noreply.github.com>
2022-05-02 11:16:58 +0200

    Use the new pycolmap.Sift interface (#181)
    * Use the new pycolmap.Sift interface with GPU support
    * Keep the root normalization
    * Bump the minimum pycolmap version

--- hloc/extractors/dog.py
+++ hloc/extractors/dog.py
@@ -65,13 +65,9 @@ def sift_to_rootsift(x):
 
 class DoG(BaseModel):
     default_conf = {
-        'vlfeat': {
-            'num_octaves': 4,
-            'octave_resolution': 3,
+        'options': {
             'first_octave': 0,
-            'edge_thresh': 10,
-            'peak_thresh': 0.01,
-            'upright': False,
+            'peak_threshold': 0.01,
         },
         'descriptor': 'rootsift',
         'max_keypoints': -1,
@@ -87,17 +83,41 @@ class DoG(BaseModel):
         elif conf['descriptor'] not in ['sift', 'rootsift']:
             raise ValueError(f'Unknown descriptor: {conf["descriptor"]}')
 
+        self.sift = None  # lazily instantiated on the first image
+        self.device = torch.device('cpu')
+
+    def to(self, *args, **kwargs):
+        device = kwargs.get('device')
+        if device is None:
+            match = [a for a in args if isinstance(a, (torch.device, str))]
+            if len(match) > 0:
+                device = match[0]
+        if device is not None:
+            self.device = torch.device(device)
+        return super().to(*args, **kwargs)
+
     def _forward(self, data):
         image = data['image']
         image_np = image.cpu().numpy()[0, 0]
         assert image.shape[1] == 1
         assert image_np.min() >= -EPS and image_np.max() <= 1 + EPS
 
-        keypoints, scores, descriptors = pycolmap.extract_sift(
-            image_np, **self.conf['vlfeat']
-        )
+        if self.sift is None:
+            use_gpu = pycolmap.has_cuda and self.device.type == 'cuda'
+            options = {**self.conf['options']}
+            if self.conf['descriptor'] == 'rootsift':
+                options['normalization'] = pycolmap.Normalization.L1_ROOT
+            else:
+                options['normalization'] = pycolmap.Normalization.L2
+            self.sift = pycolmap.Sift(
+                options=pycolmap.SiftExtractionOptions(options),
+                device=getattr(pycolmap.Device, 'cuda' if use_gpu else 'cpu'))
+
+        keypoints, scores, descriptors = self.sift.extract(image_np)
 
         if self.conf['descriptor'] in ['sift', 'rootsift']:
+            # We still renormalize because COLMAP does not normalize well,
+            # maybe due to numerical errors
             if self.conf['descriptor'] == 'rootsift':
                 descriptors = sift_to_rootsift(descriptors)
             descriptors = torch.from_numpy(descriptors)
