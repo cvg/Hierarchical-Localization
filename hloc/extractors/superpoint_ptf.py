111086cfe90e86e4e50c827954efed1ac18bc364
iMeleon <scentofdarkness@gmail.com>
2022-07-25 19:00:12 +0300

    Create superpoint_ptf.py
    implement superpointPTF in HL reconstruction pipeline

---
+++ hloc/extractors/superpoint_ptf.py
@@ -0,0 +1,224 @@
+import torch
+import numpy as np
+
+from ..utils.base_model import BaseModel
+
+
+class SuperPointNet(torch.nn.Module):
+    def __init__(self):
+        super(SuperPointNet, self).__init__()
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        self.cell = 8
+        self.nms_dist = 4
+        self.conf_thresh = 0.1
+        self.border_remove = 4
+        c1, c2, c3, c4, c5, d1 = 64, 64, 128, 128, 256, 256
+        # Shared Encoder.
+        self.conv1a = torch.nn.Conv2d(1, c1, kernel_size=3, stride=1, padding=1)
+        self.conv1b = torch.nn.Conv2d(c1, c1, kernel_size=3, stride=1, padding=1)
+        self.conv2a = torch.nn.Conv2d(c1, c2, kernel_size=3, stride=1, padding=1)
+        self.conv2b = torch.nn.Conv2d(c2, c2, kernel_size=3, stride=1, padding=1)
+        self.conv3a = torch.nn.Conv2d(c2, c3, kernel_size=3, stride=1, padding=1)
+        self.conv3b = torch.nn.Conv2d(c3, c3, kernel_size=3, stride=1, padding=1)
+        self.conv4a = torch.nn.Conv2d(c3, c4, kernel_size=3, stride=1, padding=1)
+        self.conv4b = torch.nn.Conv2d(c4, c4, kernel_size=3, stride=1, padding=1)
+        # Detector Head.
+        self.convPa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+        self.convPb = torch.nn.Conv2d(c5, 65, kernel_size=1, stride=1, padding=0)
+        # Descriptor Head.
+        self.convDa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+        self.convDb = torch.nn.Conv2d(c5, d1, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        """Forward pass that jointly computes unprocessed point and descriptor
+        tensors.
+        Input
+          x: Image pytorch tensor shaped N x 1 x H x W.
+        Output
+          semi: Output point pytorch tensor shaped N x 65 x H/8 x W/8.
+          desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
+        """
+        # Shared Encoder.
+        x = self.relu(self.conv1a(x))
+        x = self.relu(self.conv1b(x))
+        x = self.pool(x)
+        x = self.relu(self.conv2a(x))
+        x = self.relu(self.conv2b(x))
+        x = self.pool(x)
+        x = self.relu(self.conv3a(x))
+        x = self.relu(self.conv3b(x))
+        x = self.pool(x)
+        x = self.relu(self.conv4a(x))
+        x = self.relu(self.conv4b(x))
+        # Detector Head.
+        cPa = self.relu(self.convPa(x))
+        semi = self.convPb(cPa)
+
+        # Descriptor Head.
+        cDa = self.relu(self.convDa(x))
+        desc = self.convDb(cDa)
+        dn = torch.norm(desc, p=2, dim=1)  # Compute the norm.
+        desc = desc.div(torch.unsqueeze(dn, 1))  # Divide by norm to normalize.
+        return semi, desc
+
+    def nms_fast(self, in_corners, H, W, dist_thresh):
+        """
+        Run a faster approximate Non-Max-Suppression on numpy corners shaped:
+          3xN [x_i,y_i,conf_i]^T
+        Algo summary: Create a grid sized HxW. Assign each corner location a 1, rest
+        are zeros. Iterate through all the 1's and convert them either to -1 or 0.
+        Suppress points by setting nearby values to 0.
+        Grid Value Legend:
+        -1 : Kept.
+         0 : Empty or suppressed.
+         1 : To be processed (converted to either kept or supressed).
+        NOTE: The NMS first rounds points to integers, so NMS distance might not
+        be exactly dist_thresh. It also assumes points are within image boundaries.
+        Inputs
+          in_corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
+          H - Image height.
+          W - Image width.
+          dist_thresh - Distance to suppress, measured as an infinty norm distance.
+        Returns
+          nmsed_corners - 3xN numpy matrix with surviving corners.
+          nmsed_inds - N length numpy vector with surviving corner indices.
+        """
+        grid = np.zeros((H, W)).astype(int)  # Track NMS data.
+        inds = np.zeros((H, W)).astype(int)  # Store indices of points.
+        # Sort by confidence and round to nearest int.
+        inds1 = np.argsort(-in_corners[2, :])
+        corners = in_corners[:, inds1]
+        rcorners = corners[:2, :].round().astype(int)  # Rounded corners.
+        # Check for edge case of 0 or 1 corners.
+        if rcorners.shape[1] == 0:
+            return np.zeros((3, 0)).astype(int), np.zeros(0).astype(int)
+        if rcorners.shape[1] == 1:
+            out = np.vstack((rcorners, in_corners[2])).reshape(3, 1)
+            return out, np.zeros((1)).astype(int)
+        # Initialize the grid.
+        for i, rc in enumerate(rcorners.T):
+            grid[rcorners[1, i], rcorners[0, i]] = 1
+            inds[rcorners[1, i], rcorners[0, i]] = i
+        # Pad the border of the grid, so that we can NMS points near the border.
+        pad = dist_thresh
+        grid = np.pad(grid, ((pad, pad), (pad, pad)), mode="constant")
+        # Iterate through points, highest to lowest conf, suppress neighborhood.
+        count = 0
+        for i, rc in enumerate(rcorners.T):
+            # Account for top and left padding.
+            pt = (rc[0] + pad, rc[1] + pad)
+            if grid[pt[1], pt[0]] == 1:  # If not yet suppressed.
+                grid[pt[1] - pad : pt[1] + pad + 1, pt[0] - pad : pt[0] + pad + 1] = 0
+                grid[pt[1], pt[0]] = -1
+                count += 1
+        # Get all surviving -1's and return sorted array of remaining corners.
+        keepy, keepx = np.where(grid == -1)
+        keepy, keepx = keepy - pad, keepx - pad
+        inds_keep = inds[keepy, keepx]
+        out = corners[:, inds_keep]
+        values = out[-1, :]
+        inds2 = np.argsort(-values)
+        out = out[:, inds2]
+        out_inds = inds1[inds_keep[inds2]]
+        return out, out_inds
+
+    def inf_point(self, data):
+        """Process a numpy image to extract points and descriptors.
+        Input
+          img - HxW numpy float32 input image in range [0,1].
+        Output
+          corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
+          desc - 256xN numpy array of corresponding unit normalized descriptors.
+          heatmap - HxW numpy heatmap in range [0,1] of point confidences.
+        """
+        # assert img.ndim == 2, "Image must be grayscale."
+        # assert img.dtype == np.float32, "Image must be float32."
+        # H, W = img.shape[0], img.shape[1]
+        # inp = img.copy()
+        # inp = inp.reshape(1, H, W)
+        # inp = torch.from_numpy(inp)
+        # inp = torch.autograd.Variable(inp).view(1, 1, H, W)
+
+        # Forward pass of network.
+        inp = data["image"]
+        H, W = inp.shape[2], inp.shape[3]
+        if self.cuda:
+            inp = inp.cuda()
+        outs = self.forward(inp)
+        semi, coarse_desc = outs[0], outs[1]
+        # Convert pytorch -> numpy.
+        semi = semi.data.cpu().numpy().squeeze()
+        # --- Process points.
+        dense = np.exp(semi)  # Softmax.
+        dense = dense / (np.sum(dense, axis=0) + 0.00001)  # Should sum to 1.
+        # Remove dustbin.
+        nodust = dense[:-1, :, :]
+        # Reshape to get full resolution heatmap.
+        Hc = int(H / self.cell)
+        Wc = int(W / self.cell)
+        nodust = nodust.transpose(1, 2, 0)
+        heatmap = np.reshape(nodust, [Hc, Wc, self.cell, self.cell])
+        heatmap = np.transpose(heatmap, [0, 2, 1, 3])
+        heatmap = np.reshape(heatmap, [Hc * self.cell, Wc * self.cell])
+        xs, ys = np.where(heatmap >= self.conf_thresh)  # Confidence threshold.
+        if len(xs) == 0:
+            return np.zeros((3, 0)), None, None
+        pts = np.zeros((3, len(xs)))  # Populate point data sized 3xN.
+        pts[0, :] = ys
+        pts[1, :] = xs
+        pts[2, :] = heatmap[xs, ys]
+        pts, _ = self.nms_fast(pts, H, W, dist_thresh=self.nms_dist)  # Apply NMS.
+        inds = np.argsort(pts[2, :])
+        pts = pts[:, inds[::-1]]  # Sort by confidence.
+        # Remove points along border.
+        bord = self.border_remove
+        toremoveW = np.logical_or(pts[0, :] < bord, pts[0, :] >= (W - bord))
+        toremoveH = np.logical_or(pts[1, :] < bord, pts[1, :] >= (H - bord))
+        toremove = np.logical_or(toremoveW, toremoveH)
+        pts = pts[:, ~toremove]
+        # --- Process descriptor.
+        D = coarse_desc.shape[1]
+        if pts.shape[1] == 0:
+            desc = np.zeros((D, 0))
+        else:
+            samp_pts = torch.from_numpy(pts[:2, :].copy())
+            samp_pts[0, :] = (samp_pts[0, :] / (float(W) / 2.0)) - 1.0
+            samp_pts[1, :] = (samp_pts[1, :] / (float(H) / 2.0)) - 1.0
+            samp_pts = samp_pts.transpose(0, 1).contiguous()
+            samp_pts = samp_pts.view(1, 1, -1, 2)
+            samp_pts = samp_pts.float()
+            if self.cuda:
+                samp_pts = samp_pts.cuda()
+            desc = torch.nn.functional.grid_sample(coarse_desc, samp_pts)
+            desc = desc.data.cpu().numpy().reshape(D, -1)
+            desc /= np.linalg.norm(desc, axis=0)[np.newaxis, :]
+        pred = {}
+        if self.cuda:
+            pred["keypoints"] = [torch.Tensor(np.swapaxes(pts[:2, :], 0, 1)).cuda()]
+            pred["scores"] = [torch.Tensor(pts[2, :].reshape(-1)).cuda()]
+            pred["descriptors"] = [torch.Tensor(desc).cuda()]
+        else:
+            pred["keypoints"] = [torch.Tensor(np.swapaxes(pts[:2, :], 0, 1))]
+            pred["scores"] = [torch.Tensor(pts[2, :].reshape(-1))]
+            pred["descriptors"] = [torch.Tensor(desc)]
+        return pred
+
+
+class SuperPointPTF(BaseModel):
+    default_conf = {
+        "nms_radius": 4,
+        "keypoint_threshold": 0.005,
+        "max_keypoints": -1,
+        "remove_borders": 4,
+        "fix_sampling": False,
+    }
+    required_inputs = ["image"]
+    detection_noise = 2.0
+
+    def _init(self, conf):
+        self.net = SuperPointNet()
+        self.net.load_state_dict(torch.load(conf["model_pth"]))
+
+    def _forward(self, data):
+        return self.net.inf_point(data)
