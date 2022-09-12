353905feda7607d4c19d59688ee46d77ffcf25a0
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2022-09-12 23:02:08 +0200

    Remove monkeypatch of SuperPoint sample_descriptors

--- hloc/extractors/superpoint.py
+++ hloc/extractors/superpoint.py
@@ -8,27 +8,6 @@ sys.path.append(str(Path(__file__).parent / '../../third_party'))
 from SuperGluePretrainedNetwork.models import superpoint  # noqa E402
 
 
-# torch>=1.10 breaks the original version check.
-# We monkeypatch the function until this PR is merged:
-# https://github.com/magicleap/SuperGluePretrainedNetwork/pull/104
-def sample_descriptors(keypoints, descriptors, s: int = 8):
-    """ Interpolate descriptors at keypoint locations """
-    b, c, h, w = descriptors.shape
-    keypoints = keypoints - s / 2 + 0.5
-    keypoints /= torch.tensor([(w*s - s/2 - 0.5), (h*s - s/2 - 0.5)],
-                              ).to(keypoints)[None]
-    keypoints = keypoints*2 - 1  # normalize to (-1, 1)
-    args = {'align_corners': True} if torch.__version__ >= '1.3' else {}
-    descriptors = torch.nn.functional.grid_sample(
-        descriptors, keypoints.view(b, 1, -1, 2), mode='bilinear', **args)
-    descriptors = torch.nn.functional.normalize(
-        descriptors.reshape(b, c, -1), p=2, dim=1)
-    return descriptors
-
-
-superpoint.sample_descriptors = sample_descriptors
-
-
 # The original keypoint sampling is incorrect. We patch it here but
 # we don't fix it upstream to not impact exisiting evaluations.
 def sample_descriptors_fix_sampling(keypoints, descriptors, s: int = 8):
