5b4e0e9ad3c024613a186d478115ecc996327334
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2022-10-08 16:06:41 +0200

    Parallel reading and writing

--- hloc/match_features.py
+++ hloc/match_features.py
@@ -2,6 +2,9 @@ import argparse
 from typing import Union, Optional, Dict, List, Tuple
 from pathlib import Path
 import pprint
+from queue import Queue
+from threading import Thread
+from functools import partial
 from tqdm import tqdm
 import h5py
 import torch
@@ -64,6 +67,71 @@ confs = {
         },
     }
 }
+
+
+class WorkQueue():
+    def __init__(self, work_fn, num_threads=1):
+        self.queue = Queue(num_threads)
+        self.threads = [
+            Thread(target=self.thread_fn, args=(work_fn,))
+            for _ in range(num_threads)
+        ]
+        for thread in self.threads:
+            thread.start()
+
+    def join(self):
+        for thread in self.threads:
+            self.queue.put(None)
+        for thread in self.threads:
+            thread.join()
+
+    def thread_fn(self, work_fn):
+        item = self.queue.get()
+        while item is not None:
+            work_fn(item)
+            item = self.queue.get()
+
+    def put(self, data):
+        self.queue.put(data)
+
+
+class FeaturePairsDataset(torch.utils.data.Dataset):
+    def __init__(self, pairs, feature_path_q, feature_path_r):
+        self.pairs = pairs
+        self.feature_path_q = feature_path_q
+        self.feature_path_r = feature_path_r
+
+    def __getitem__(self, idx):
+        name0, name1 = self.pairs[idx]
+        data = {}
+        with h5py.File(self.feature_path_q, 'r') as fd:
+            grp = fd[name0]
+            for k, v in grp.items():
+                data[k+'0'] = torch.from_numpy(v.__array__()).float()
+            # some matchers might expect an image but only use its size
+            data['image0'] = torch.empty((1,)+tuple(grp['image_size'])[::-1])
+        with h5py.File(self.feature_path_r, 'r') as fd:
+            grp = fd[name1]
+            for k, v in grp.items():
+                data[k+'1'] = torch.from_numpy(v.__array__()).float()
+            data['image1'] = torch.empty((1,)+tuple(grp['image_size'])[::-1])
+        return data
+
+    def __len__(self):
+        return len(self.pairs)
+
+
+def writer_fn(inp, match_path):
+    pair, pred = inp
+    with h5py.File(str(match_path), 'a', libver='latest') as fd:
+        if pair in fd:
+            del fd[pair]
+        grp = fd.create_group(pair)
+        matches = pred['matches0'][0].cpu().short().numpy()
+        grp.create_dataset('matches0', data=matches)
+        if 'matching_scores0' in pred:
+            scores = pred['matching_scores0'][0].cpu().half().numpy()
+            grp.create_dataset('matching_scores0', data=scores)
 
 
 def main(conf: Dict,
@@ -143,34 +211,18 @@ def match_from_paths(conf: Dict,
     Model = dynamic_load(matchers, conf['model']['name'])
     model = Model(conf['model']).eval().to(device)
 
-    for (name0, name1) in tqdm(pairs, smoothing=.1):
-        data = {}
-        with h5py.File(str(feature_path_q), 'r', libver='latest') as fd:
-            grp = fd[name0]
-            for k, v in grp.items():
-                data[k+'0'] = torch.from_numpy(v.__array__()).float().to(device)
-            # some matchers might expect an image but only use its size
-            data['image0'] = torch.empty((1,)+tuple(grp['image_size'])[::-1])
-        with h5py.File(str(feature_path_ref), 'r', libver='latest') as fd:
-            grp = fd[name1]
-            for k, v in grp.items():
-                data[k+'1'] = torch.from_numpy(v.__array__()).float().to(device)
-            data['image1'] = torch.empty((1,)+tuple(grp['image_size'])[::-1])
-        data = {k: v[None] for k, v in data.items()}
+    dataset = FeaturePairsDataset(pairs, feature_path_q, feature_path_ref)
+    loader = torch.utils.data.DataLoader(
+        dataset, num_workers=5, batch_size=1, shuffle=False, pin_memory=True)
+    writer_queue = WorkQueue(partial(writer_fn, match_path=match_path), 5)
 
+    for idx, data in enumerate(tqdm(loader, smoothing=.1)):
+        data = {k: v if k.startswith('image')
+                else v.to(device, non_blocking=True) for k, v in data.items()}
         pred = model(data)
-        pair = names_to_pair(name0, name1)
-        with h5py.File(str(match_path), 'a', libver='latest') as fd:
-            if pair in fd:
-                del fd[pair]
-            grp = fd.create_group(pair)
-            matches = pred['matches0'][0].cpu().short().numpy()
-            grp.create_dataset('matches0', data=matches)
-
-            if 'matching_scores0' in pred:
-                scores = pred['matching_scores0'][0].cpu().half().numpy()
-                grp.create_dataset('matching_scores0', data=scores)
-
+        pair = names_to_pair(*pairs[idx])
+        writer_queue.put((pair, pred))
+    writer_queue.join()
     logger.info('Finished exporting matches.')
 
 
