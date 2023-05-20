acfdd0466ceb68005a7febcc3febcbae79687027
Realcat <realcat@126.com>
2023-05-20 13:08:35 +0000

    add: new matcher topicfm

--- hloc/matchers/topicfm.py
+++ hloc/matchers/topicfm.py
@@ -4,28 +4,29 @@ from ..utils.base_model import BaseModel
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent / '../../third_party'))
-from ASpanFormer.src.ASpanFormer.aspanformer import ASpanFormer as _ASpanFormer
-from ASpanFormer.src.config.default import get_cfg_defaults
-from ASpanFormer.src.utils.misc import lower_config
-from ASpanFormer.demo import demo_utils 
-aspanformer_path = Path(__file__).parent / '../../third_party/ASpanFormer'
+from TopicFM.src.models.topicfm import TopicFM as _TopicFM
+from TopicFM.src.config.default import get_cfg_defaults
+from TopicFM.src.utils.misc import lower_config
+# from TopicFM.demo import demo_utils 
+topicfm_path = Path(__file__).parent / '../../third_party/TopicFM'
 
-class ASpanFormer(BaseModel):
+class TopicFM(BaseModel):
     default_conf = {
         'weights': 'outdoor',
         'match_threshold': 0.2,
-        'config_path': aspanformer_path / 'configs/aspan/outdoor/aspan_test.py',
+        'config_path': topicfm_path / 'configs/aspan/outdoor/aspan_test.py',
     }
     required_inputs = [
         'image0',
         'image1'
     ]
+    # todo: refer to :third_party/TopicFM/viz/methods/topicfm.py
     def _init(self, conf):
         config = get_cfg_defaults()
         config.merge_from_file(conf['config_path'])
         _config = lower_config(config)
-        self.net = _ASpanFormer(config=_config['aspan'])
-        weight_path =  aspanformer_path / 'weights/{}.ckpt'.format(conf['weights'])
+        self.net = _TopicFM(config=_config['aspan'])
+        weight_path =  topicfm_path / 'pretrained/model_best.ckpt'
         state_dict = torch.load(str(weight_path), map_location='cpu')['state_dict']
         self.net.load_state_dict(state_dict, strict=False)
 
