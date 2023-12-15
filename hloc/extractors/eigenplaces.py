42e11d3999587af63b865cce6bd0fadbd81dba7b
gmberton <berton.gabri@gmail.com>
2023-12-15 16:42:54 +0100

    Added EigenPlaces for retrieval

--- hloc/extractors/eigenplaces.py
+++ hloc/extractors/eigenplaces.py
@@ -1,17 +1,26 @@
 '''
-Code for loading models trained with CosPlace as a global features extractor
-for geolocalization through image retrieval.
+Code for loading models trained with EigenPlaces (or CosPlace) as a global
+features extractor for geolocalization through image retrieval.
 Multiple models are available with different backbones. Below is a summary of
 models available (backbone : list of available output descriptors
 dimensionality). For example you can use a model based on a ResNet50 with
 descriptors dimensionality 1024.
+
+EigenPlaces trained models:
+    ResNet18:  [     256, 512]
+    ResNet50:  [128, 256, 512, 2048]
+    ResNet101: [128, 256, 512, 2048]
+    VGG16:     [     512]
+
+CosPlace trained models:
     ResNet18:  [32, 64, 128, 256, 512]
     ResNet50:  [32, 64, 128, 256, 512, 1024, 2048]
     ResNet101: [32, 64, 128, 256, 512, 1024, 2048]
     ResNet152: [32, 64, 128, 256, 512, 1024, 2048]
     VGG16:     [    64, 128, 256, 512]
 
-CosPlace paper: https://arxiv.org/abs/2204.02287
+EigenPlaces paper (ICCV 2023): https://arxiv.org/abs/2308.10832
+CosPlace paper (CVPR 2022): https://arxiv.org/abs/2204.02287
 '''
 
 import torch
@@ -20,15 +29,16 @@ import torchvision.transforms as tvf
 from ..utils.base_model import BaseModel
 
 
-class CosPlace(BaseModel):
+class EigenPlaces(BaseModel):
     default_conf = {
-        'backbone': 'ResNet50',
+        'variant': 'EigenPlaces',
+        'backbone': 'ResNet101',
         'fc_output_dim' : 2048
     }
     required_inputs = ['image']
     def _init(self, conf):
         self.net = torch.hub.load(
-            'gmberton/CosPlace',
+            'gmberton/' + conf['variant'],
             'get_trained_model',
             backbone=conf['backbone'],
             fc_output_dim=conf['fc_output_dim']
